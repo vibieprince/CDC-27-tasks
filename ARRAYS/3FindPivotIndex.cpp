8ed6d9517e8fb220c0f26f97f4e9a6d6ef0be240
Prince Singh <princesingh15122004@gmail.com>
2025-01-23 20:20:56 +0530

    3,4 task done

---
+++ ARRAYS/3FindPivotIndex.cpp
@@ -0,0 +1,19 @@
+// Find Pivot Index (724)
+#include<iostream>
+#include<vector>
+#include <numeric>
+using namespace std;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = accumulate(nums.begin(),nums.end(),0);
+        int leftTotal = 0;
+        for(int i=0;i<nums.size();i++){
+            int rightTotal = total-leftTotal-nums[i];
+            if(rightTotal==leftTotal)
+                return i;
+            leftTotal += nums[i];
+        }
+        return -1;
+    }
+};
